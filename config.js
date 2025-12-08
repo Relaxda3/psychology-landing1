3406b3f9b5f52b49be5bbd494c0800481386a2dc
Relaxda3 <106255335+Relaxda3@users.noreply.github.com>
2025-12-08 17:39:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   pageTitle: "Авторская практика от психолога",
   mainTitle: "Авторская практика от психолога-гипнотерапевта",
   highlight1: "ТЕХНИКА БЫСТРОГО ВОССТАНОВЛЕНИЯ",
-  subtitle: "15 минут, чтобы сбросить стресс",
+  subtitle: "15 часов, чтобы сбросить стресс",
   newPrice: "98 грн",
   oldPrice: "2198 грн",
   highlight2: "Цена одного кофе вместо сессии психолога",
@@ -11,3 +11,4 @@ const CONFIG = {
   // background может быть относительным путём (content-images/background.jpg) или внешним URL
   background: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1470&q=80"
 };
+
